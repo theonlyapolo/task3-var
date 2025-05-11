e61419883cdcc6a2f229f95592fd57abe7aa90ea
Apolo <paolacrisnvs@gmail.com>
2025-05-11 08:48:13 -0300

    Add project files.

---
+++ task3-var/Program.cs
@@ -0,0 +1,10 @@
+﻿string nome = "Apolo";
+int idade = 16;
+double altura = 1.72;
+string estado_civil = "Solteiro";
+
+// como o tipo do projeto é console, ignorei a parte do Main e segui com as exibições de informação
+Console.WriteLine("Nome: " + nome);
+Console.WriteLine("Idade: " + idade);
+Console.WriteLine("Altura: " + altura);
+Console.WriteLine("Estado Civil: " + estado_civil);
